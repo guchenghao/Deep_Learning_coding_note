f8c526b9566f3e32c9440f3b1942ba3eec9b41f7
辜承昊 <1754926878@qq.com>
2024-12-04 17:02:14 +0900

    update No。206

--- Leetcode/Linkedlist/No.206_reverse_Linkedlist.py
+++ Leetcode/Linkedlist/No.206_reverse_Linkedlist.py
@@ -17,3 +17,24 @@ class Solution(object):
             cur = temp
 
         return prev
+
+
+# * 递归写法
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        def reverse(cur, prev):
+
+            if not cur:
+                return prev
+
+            temp = cur.next
+            cur.next = prev
+
+            return reverse(temp, cur)
+
+        return reverse(head, None)
