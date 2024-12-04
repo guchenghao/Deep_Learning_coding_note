1a55a9ee12bf66e32bd38654c1697f99cae20b1d
辜承昊 <1754926878@qq.com>
2024-12-04 18:01:43 +0900

    update No.24

---
+++ Leetcode/Linkedlist/No.24_exchange_node_of_Linkedlist(虚拟头节点).py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy_head = ListNode(next=head)
+
+        cur = dummy_head
+
+        while cur.next and cur.next.next:
+            temp1 = cur.next
+            temp2 = cur.next.next.next
+
+            cur.next = cur.next.next
+
+            cur.next.next = temp1
+
+            temp1.next = temp2
+
+            cur = cur.next.next
+
+        return dummy_head.next
