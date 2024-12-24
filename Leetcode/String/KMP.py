6c43a2a9b54c7a65de523f8613c23cca8124d2b0
辜承昊 <1754926878@qq.com>
2024-12-24 16:01:24 +0900

    new KMP and Leetcode No.28

---
+++ Leetcode/String/KMP.py
@@ -0,0 +1,45 @@
+mode_str = "aabaaf"
+target_str = "aabaabaafaabaaf"
+next = [0] * len(mode_str)
+
+
+# * 获得前缀数组
+def getPrefix_arr(next, mode_str):
+    j = 0   # * 前缀末尾
+    next[0] = 0
+    
+    
+    for i in range(1, len(mode_str)):
+        while j > 0 and mode_str[i] != mode_str[j]:
+            j = next[j - 1]
+            
+        
+        if mode_str[i] == mode_str[j]:
+            j += 1
+        
+        
+        next[i] = j
+    
+    
+    return next
+
+next = getPrefix_arr(next, mode_str)
+
+
+print(next)
+
+j = 0
+
+for i in range(len(target_str)):
+    # * 如果不匹配的话, j进行回退
+    while j > 0 and mode_str[j] != target_str[i]:
+         j = next[j - 1]
+        
+    
+    if mode_str[j] == target_str[i]:  # * 如果匹配，i++和j++
+        j += 1
+    
+    
+    if j == len(mode_str):
+        print("true")
+        break
---
+++ Leetcode/String/No.28_index_matching_string(KMP).py
@@ -0,0 +1,38 @@
+class Solution(object):
+    # * 计算KMP算法中前缀数组
+    def getPrefix_arr(self, next, mode_str):
+        j = 0  # * 前缀末尾
+        next[0] = 0
+
+        for i in range(1, len(mode_str)):
+            while j > 0 and mode_str[i] != mode_str[j]:
+                j = next[j - 1]
+
+            if mode_str[i] == mode_str[j]:
+                j += 1
+
+            next[i] = j
+
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        next = [0] * len(needle)
+
+        self.getPrefix_arr(next, needle)
+
+        j = 0
+
+        for i in range(len(haystack)):
+            while j > 0 and needle[j] != haystack[i]:
+                j = next[j - 1]
+
+            if needle[j] == haystack[i]:
+                j += 1
+
+            if j == len(needle):
+                return i - len(needle) + 1
+
+        return -1
